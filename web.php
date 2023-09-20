7d79d9332ee0236e11ef3f149bafe8663c84add3
abujafar01 <126306175+abujafar01@users.noreply.github.com>
2023-09-20 21:40:37 +0600

    Add files via upload
    Code correct ase kina .ami run korte parcina..Route r Controller er modde

---
+++ web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/', 'SiteController@Home');
+
+
+Route::get('/about', 'SiteController@About');
+
+
+Route::get('/contact', 'SiteController@Contact');
